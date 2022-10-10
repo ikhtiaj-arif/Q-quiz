53029be8bf21e83c80635e19de70a2adce8b4e39
AnGon777 <ikhtiaj.arif@gmail.com>
2022-10-10 21:18:11 +0600

    added components and tailwind

---
+++ src/components/statistics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const statistics = () => {
+    return (
+        <div>
+            <h1>stasts</h1>
+            
+        </div>
+    );
+};
+
+export default statistics;
