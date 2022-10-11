c5a417ebfc78b024b5ccf3b9a0aa23565ce3fc23
AnGon777 <ikhtiaj.arif@gmail.com>
2022-10-11 16:50:15 +0600

    responsive heading section added

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Heading = () => {
     return (
-        <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fuga doloribus ipsam esse dolor officiis eius expedita dolorem, laudantium ipsa iure sed, nemo adipisci, ipsum suscipit fugiat in ad porro accusamus itaque eligendi sit sequi? Optio, nihil! Praesentium, illum.</h2>
+        <div className='bg-gray-200 flex justify-around h-1/2'>
+            <img className='w-2/4 md:w-1/4 mx-6 my-3' src="https://www.mastersoftwaresolutions.com/wp-content/uploads/2019/06/bnr-36.png" alt="" />
+            <div className='mt-8 md:my-16'>
+                <h2 className='text-2xl md:text-4xl font-bold text-red-500'>Lets Test Your Skills!</h2>
+                <p className='py-4 text-xl font-medium text-cyan-600'>Chose Any Of The Topic Below</p>
+                <p className='text-2xl md:text-4xl font-bold text-red-500'>Good Luck!!</p>
+            </div>
         </div>
     );
 };
